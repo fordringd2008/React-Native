77c2cd9cd84d9aea3725d0142e383bfc76061509
3irobotics <dingfd1127@3irobotics.com>
2020-03-14 08:55:12 +0800

    github

--- React Demo/HelloWorld1/App.js
+++ React Demo/HelloWorld1/App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,101 +14,104 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
+  Alert,
+  Button,
+  TouchableWithoutFeedbackComponent,
+  TouchableWithoutFeedback,
+  Image,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+
+// export default class App extends Component {
+//   _onPressButton() {
+//     Alert.alert('You tapped the button!');
+//   }
+//   render() {
+//     return (
+//       <View style={[styles.container, styles.flex]}>
+//         {/* <Button
+//           onPress={this._onPressButton}
+//           title="Click me"
+//           styles={styles.text}
+//         /> */}
+//         <TouchableWithoutFeedback
+//           onPress={this._onPressButton}
+//           style={styles.container}>
+//           <Text>搜索</Text>
+//         </TouchableWithoutFeedback>
+//       </View>
+//     );
+//   }
+// }
+
+export default class App extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!');
+  }
+  render() {
+    return (
+      <ScrollView style={{flex:1}}>
+        <Text style={{fontSize: 96}}>Scroll view</Text>
+        <Image
+          source={{
+            uri:
+              'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584030596588&di=4339c3e1783c3d6379d7fb8e903a4b71&imgtype=0&src=http%3A%2F%2Fcdn.qiancipai.com%2F190305170514872174.jpg',
+            width: 164,
+            height: 164,
+          }}
+        />
+      </ScrollView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  text: {
+    width: 45,
+    height: 50,
+    marginLeft: 55,
+    marginTop: 55,
+    flex: 1,
+    backgroundColor: 'red',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  container: {
+    flexDirection: 'row',
+    height: 45,
+    marginTop: 88,
   },
-  body: {
-    backgroundColor: Colors.white,
+  flex: {
+    flex: 1,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  input: {
+    height: 45,
+    borderWidth: 1,
+    marginLeft: 5,
+    paddingLeft: 5,
+    marginRight: 5,
+    borderColor: '#CCCCCC',
+    borderRadius: 4,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  btn: {
+    width: 55,
+    marginLeft: 5,
+    marginRight: 5,
+    borderColor: '#234CCC',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    alignItems: 'center',
+    height: 45,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  search: {
+    color: '#FFF',
+    fontSize: 15,
+    fontWeight: 'bold',
   },
 });
 
-export default App;
+// export default App;
